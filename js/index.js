66393a74fb52ed48d3773bfeb0d9e67c6e8b718b
orjocota <orjocota@outlook.com>
2021-12-03 20:43:39 -0500

    Primea subida de archivos

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// Codigo para el menu "Active"//
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll('a');
+const menuLength = menuItem.length
+for (let i =0; i<menuLength; i++){
+    if(menuItem[i].href === currentLocation){
+        menuItem[i].className = "active"
+
+    }
+}
+//codigo del Menu Hamburgesa//
